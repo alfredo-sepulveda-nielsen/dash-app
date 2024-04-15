34c56b801a82cbe90133a9e055f24bb3754d76a5
ALFREDO SEPULVEDA <Alfredo.Sepulveda@nielsen.com>
2024-04-14 21:27:13 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 19:09:58 2024
+
+@author: alsepulv2001
+"""
+
+from dash import Dash, dcc, html, Input, Output, callback
+import os
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+app.layout = html.Div([
+    html.H1('Hello World'),
+    dcc.Dropdown(['LA', 'NYC', 'MTL'],
+        'LA',
+        id='dropdown'
+    ),
+    html.Div(id='display-value')
+])
+
+@callback(Output('display-value', 'children'), Input('dropdown', 'value'))
+def display_value(value):
+    return f'You have selected {value}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
